42c62a29a8cd4f997b75c70d98e8b3772797ec30
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-12-19 21:30:41 +0000

    plugins and middleware ammended

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,4 @@
-module.exports = {
-     name: "strapi::body",
-    config: {
-      formLimit: "256mb", // modify form body
-      jsonLimit: "256mb", // modify JSON body
-      textLimit: "256mb", // modify text body
-      formidable: {
-        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
-      },
-    },
-},[
+module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
@@ -18,4 +8,15 @@ module.exports = {
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
-];
+],
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  }
